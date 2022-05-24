62d427776d3dd7948982a7c80a1d9ec26fd7ae52
David Kistauri <davidkis113@gmail.com>
2022-05-24 16:44:34 +0300

    Rewrite existing tests

--- talking_test.py
+++ talking_test.py
@@ -6,15 +6,21 @@ from talking import *
 
 class TestTalking:
     def test_handle_not_json(self):
-        assert handle('test') == None
+        conn = Connection('test', config('ORDER_SERVER_IP'),
+                          config('ORDER_SERVER_PORT'))
+        assert conn.handle('test') == None
 
     def test_handle_no_link(self):
+        conn = Connection('test', config('ORDER_SERVER_IP'),
+                          config('ORDER_SERVER_PORT'))
         data = '{"id": "test"}'
-        assert handle(data) == None
+        assert conn.handle(data) == None
 
     def test_handle_no_id(self):
+        conn = Connection('test', config('ORDER_SERVER_IP'),
+                          config('ORDER_SERVER_PORT'))
         data = '{"link": "test.com"}'
-        assert handle(data) == None
+        assert conn.handle(data) == None
 
     # def test_handle_proper(self):
     #     data = '{"id": "test", "link": "test.com"}'
@@ -25,8 +31,12 @@ class TestTalking:
         file = config("ID_FILE")
         ip = config("AUTH_SERVER_IP")
         port = config("AUTH_SERVER_PORT")
-        os.remove(file)
-        id = auth(ip, port)
+        auth = Auth(ip, port, file)
+        try:
+            os.remove(file)
+        except:
+            pass
+        id = auth.auth()
         assert isinstance(id, str) and len(id) != 0
 
     def test_auth_empty_file(self):
@@ -34,8 +44,9 @@ class TestTalking:
         id = None
         ip = config("AUTH_SERVER_IP")
         port = config("AUTH_SERVER_PORT")
+        auth = Auth(ip, port, file)
         with open(file, "w") as f:
             f.truncate(0)
-            id = auth(ip, port)
+            id = auth.auth()
         assert isinstance(id, str) and len(id) != 0
         os.remove(file)
